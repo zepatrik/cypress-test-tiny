9ab0107cfd74022ffc74be1d533b28e0b9c1155b
zepatrik <11patti1@gmx.de>
2018-12-10 14:19:24 +0100

    add buggy test

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,13 @@
 describe('page', () => {
-  it('works', () => {})
+  before(() => {
+    cy.visit('http://localhost:5008')
+  })
+
+  it('works', () => {
+    cy.get('[data-testId$=up]').eq(1).click()
+  })
+
+  it('fails', () => {
+    cy.get('[data-testId$=down]').eq(1).click()
+  })
 })
